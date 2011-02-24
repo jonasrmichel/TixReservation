2ff1b1a1ec0aa03c29349aee58cbd42b5d54fb6a
Kyle P <corespace@gmail.com>
2011-02-23 18:11:43 -0600

    blargblarg threads and such

--- src/TicketServer.java
+++ src/TicketServer.java
@@ -1,20 +1,19 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
+import java.net.SocketException;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class TicketServer {
 	SeatTable seatTable_;
-	static int myID;
+	int myID;
 	volatile long[] myClock = new long[Symbols.maxServers];
-	static List<Socket> otherActiveServers = new ArrayList<Socket>();
+	Socket[] connections = new Socket[Symbols.maxServers];
 	volatile PriorityQueue<Request> requests = new PriorityQueue<Request>(
 			Symbols.maxServers);
 
@@ -46,64 +45,57 @@ public class TicketServer {
 	public void doStuff() throws MaxServersReachedException {
 		ServerSocket listener = getUnusedPort(Symbols.serverList_Public);
 		ServerSocket serverListener = getUnusedPort(Symbols.serverList_Private);
+		try {
+			serverListener.setSoTimeout(0);
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		myID = serverListener.getLocalPort() - Symbols.basePort_Private;
 		myClock[myID] = 1;
+		int firstServerWeFind = -1;
 		for (int i : Symbols.serverList_Private) {
 			if (i == serverListener.getLocalPort())
 				continue;
 			try {
 				Socket server = new Socket(Symbols.ticketServer, i);
 				server.setSoTimeout(Symbols.timeout);
-				PrintStream pout = new PrintStream(server.getOutputStream());
+				PrintWriter pout = new PrintWriter(server.getOutputStream());
 				pout.println("hey " + myID + " " + 1);
 				pout.flush();
 				BufferedReader br = new BufferedReader(new InputStreamReader(
 						server.getInputStream()));
 				String line = br.readLine();
+				System.out.println(line);
+
+				new Thread(new ServerHandlerRunner(server)).start();
 				StringTokenizer st = new StringTokenizer(line);
 				String rmi = st.nextToken(); // should be ack
 				assert rmi.equals("ack");
+				int theirID = Integer.parseInt(st.nextToken());
+
+				connections[theirID] = server;
+				if (firstServerWeFind == -1)
+					firstServerWeFind = theirID;
+
 				long theirClock = Long.parseLong(st.nextToken()); // clock val
-				int theirID = i - Symbols.basePort_Private;
 				myClock[myID] = Math.max(myClock[myID], theirClock) + 1;
 				myClock[theirID] = Math.max(myClock[theirID], theirClock);
-				otherActiveServers.add(server);
+
 			} catch (IOException ex) {
 				// Server is not up... ignore.
 			}
 		}
-		if (!otherActiveServers.isEmpty()) {
-			getMutex();
+		if (firstServerWeFind != -1) {
 			try {
-				Socket firstguy = otherActiveServers.get(0);
-				PrintStream pout = new PrintStream(firstguy.getOutputStream());
+				Socket firstguy = connections[firstServerWeFind];
+				PrintWriter pout = new PrintWriter(firstguy.getOutputStream());
 				pout.println("gst " + myID + " " + myClock[myID]);
 				pout.flush();
-				BufferedReader br = new BufferedReader(new InputStreamReader(
-						firstguy.getInputStream()));
-				String line = br.readLine();
-				StringTokenizer st = new StringTokenizer(line);
-				String rmi = st.nextToken(); // should be rdy
-				assert rmi.equals("rdy");
-				int theirID = Integer.parseInt(st.nextToken());
-				long theirClock = Long.parseLong(st.nextToken());
-				myClock[myID] = Math.max(myClock[myID], theirClock) + 1;
-				myClock[theirID] = Math.max(myClock[theirID], theirClock);
-				int numInTable = Integer.parseInt(st.nextToken());
-				seatTable_.setCount(numInTable);
-				for (int i = 0; i < numInTable; ++i) {
-					String seatEntry = br.readLine();
-					st = new StringTokenizer(seatEntry);
-					int seat = Integer.parseInt(st.nextToken());
-					String name = st.nextToken();
-					seatTable_.seats[seat] = name;
-				}
 			} catch (IOException ex) {
 				// This shouldn't happen?
 				assert false;
 			}
-			releaseMutex("null", "null");
-
 		}
 		new Thread(new ClientHandlerRunner(listener)).start();
 		while (true) {
@@ -127,8 +119,11 @@ public class TicketServer {
 
 	private void releaseMutex(String command, String name) {
 		System.out.println("releasing mutex!");
-		List<Socket> temp = new ArrayList<Socket>();
-		for (Socket s : otherActiveServers) {
+		for (int i = 0; i < connections.length; ++i) {
+			Socket s = connections[i];
+			if (s == null) {
+				continue;
+			}
 			try {
 				PrintWriter pout = new PrintWriter(s.getOutputStream());
 				pout.println("rel " + myID + " " + myClock[myID] + " "
@@ -136,10 +131,9 @@ public class TicketServer {
 				pout.flush();
 			} catch (IOException e) {
 				// Server is dead.
-				temp.add(s);
+				connections[i] = null;
 			}
 		}
-		otherActiveServers.removeAll(temp);
 		++myClock[myID];
 		synchronized (requests) {
 			requests.remove();
@@ -150,16 +144,19 @@ public class TicketServer {
 
 	private void getMutex() {
 		System.out.println("getting mutex!");
-		List<Socket> temp = new ArrayList<Socket>();
 		synchronized (myClock) {
-			for (Socket s : otherActiveServers) {
+			for (int i = 0; i < connections.length; ++i) {
+				Socket s = connections[i];
+				if (s == null) {
+					continue;
+				}
 				try {
 					PrintWriter pout = new PrintWriter(s.getOutputStream());
 					pout.println("req " + myID + " " + myClock[myID]);
 					pout.flush();
 				} catch (IOException e) {
 					// Server is dead.
-					temp.add(s);
+					connections[i] = null;
 				}
 			}
 			synchronized (requests) {
@@ -167,29 +164,9 @@ public class TicketServer {
 			}
 		}
 		++myClock[myID];
-		int theirID = -1;
-		for (Socket s : otherActiveServers) {
-			try {
-				BufferedReader din = new BufferedReader(new InputStreamReader(
-						s.getInputStream()));
-				String ok = din.readLine();
 
-				StringTokenizer st = new StringTokenizer(ok);
-				String token = st.nextToken();
-				assert token.equals("ack");
-				theirID = Integer.parseInt(st.nextToken());
-				token = st.nextToken();
-				myClock[myID] = Math.max(myClock[myID], Long.parseLong(token)) + 1;
-				myClock[theirID] = Math.max(myClock[theirID],
-						Long.parseLong(token));
-			} catch (IOException e) {
-				// Server is dead.
-				temp.add(s);
-			}
-		}
-		otherActiveServers.removeAll(temp);
 		synchronized (requests) {
-			while (requests.peek().id_ != myID) {
+			while (notMutex()) {
 				try {
 					requests.wait();
 
@@ -202,6 +179,16 @@ public class TicketServer {
 		System.out.println("got mutex!");
 	}
 
+	private boolean notMutex() {
+		if (requests.peek().id_ != myID)
+			return true;
+		for (int i = 0; i < myClock.length; ++i) {
+			if (connections[i] != null && myClock[i] < requests.peek().clock_)
+				return true;
+		}
+		return false;
+	}
+
 	public class ServerHandlerRunner implements Runnable {
 
 		protected Socket clientSocket = null;
@@ -210,66 +197,79 @@ public class TicketServer {
 			this.clientSocket = serverSocket;
 		}
 
+		BufferedReader din;
+		PrintWriter pout;
+		int theirID;
+
 		@Override
 		public void run() {
 			try {
-				BufferedReader din = new BufferedReader(new InputStreamReader(
+				din = new BufferedReader(new InputStreamReader(
 						clientSocket.getInputStream()));
-				PrintWriter pout = new PrintWriter(
-						clientSocket.getOutputStream());
+				pout = new PrintWriter(clientSocket.getOutputStream());
+				while (true) {
 
-				String getline = din.readLine();
-				StringTokenizer st = new StringTokenizer(getline);
+					String getline = din.readLine();
+					System.out.println(myClock[myID] + ": " + getline);
+					StringTokenizer st = new StringTokenizer(getline);
 
-				String rmi = st.nextToken(); // req, rel, etc
-				int theirID = Integer.parseInt(st.nextToken());
-				long theirClock = Long.parseLong(st.nextToken()); // clock val
-				System.out.println(getline);
-				if (rmi.equals("req")) { // received a request
-					synchronized (requests) {
-						requests.add(new Request(theirID, theirClock));
-					}
+					String rmi = st.nextToken(); // req, rel, etc
+					int theirID = Integer.parseInt(st.nextToken());
+					long theirClock = Long.parseLong(st.nextToken()); // clock
+																		// val
 
-					pout.println("ack " + myID + " " + myClock[myID]);
-					pout.flush();
-				} else if (rmi.equals("rel")) { // received a release
-					String mod = st.nextToken(); // reserve, delete
-					String name = st.nextToken();
+					if (rmi.equals("req")) { // received a request
+						synchronized (requests) {
+							requests.add(new Request(theirID, theirClock));
+						}
 
-					synchronized (requests) {
-						requests.remove();
-						requests.notifyAll();
-					}
-
-					if (mod.equals("res")) {
-						seatTable_.reserve(name);
-					} else if (mod.equals("del")) {
-						seatTable_.delete(name);
-					}
-				} else if (rmi.equals("hey")) { // new server
-					otherActiveServers.add(clientSocket);
-					pout.println("ack " + myID + " " + myClock[myID]);
-					pout.flush();
-				} else if (rmi.equals("gst")) {
-					pout.println("rdy " + myID + " " + myClock[myID] + " "
-							+ seatTable_.getCount());
-					pout.flush();
-					int sent = 0;
-					for (int i = 0; i < seatTable_.maxSize
-							&& sent < seatTable_.getCount(); ++i) {
-						if (seatTable_.emptySeat(seatTable_.seats[i]))
-							continue;
-						pout.println(i + " " + seatTable_.seats[i]);
+						pout.println("ack " + myID + " " + myClock[myID]);
 						pout.flush();
-						++sent;
+					} else if (rmi.equals("rel")) { // received a release
+						String mod = st.nextToken(); // reserve, delete
+						String name = st.nextToken();
+
+						synchronized (requests) {
+							requests.remove();
+							requests.notifyAll();
+						}
+
+						if (mod.equals("res")) {
+							seatTable_.reserve(name);
+						} else if (mod.equals("del")) {
+							seatTable_.delete(name);
+						}
+					} else if (rmi.equals("hey")) { // new server
+						pout.println("ack " + myID + " " + myClock[myID]);
+						pout.flush();
+						this.theirID = theirID;
+						connections[theirID] = clientSocket;
+					} else if (rmi.equals("gst")) {
+						pout.println("rdy " + myID + " " + myClock[myID] + " "
+								+ seatTable_.getCount());
+						pout.flush();
+						int sent = 0;
+						for (int i = 0; i < seatTable_.maxSize
+								&& sent < seatTable_.getCount(); ++i) {
+							if (seatTable_.emptySeat(seatTable_.seats[i]))
+								continue;
+							pout.println(i + " " + seatTable_.seats[i]);
+							pout.flush();
+							++sent;
+						}
+					}
+					synchronized (myClock) {
+						myClock[myID] = Math.max(myClock[myID], theirClock) + 1;
+						myClock[theirID] = Math.max(myClock[theirID],
+								theirClock);
 					}
 				}
-				synchronized (myClock) {
-					myClock[myID] = Math.max(myClock[myID], theirClock) + 1;
-					myClock[theirID] = Math.max(myClock[theirID], theirClock);
-				}
-			} catch (Exception e) {
+			} catch (IOException e) {
+				// Server died?
 				System.err.println(e);
+				
+				connections[theirID] = null;
+				// Let this thread die.
 			}
 		}
 	}
